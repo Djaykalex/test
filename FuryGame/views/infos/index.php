f4398e2e8afbd82ab49ebfaf8109da756c6b39e3
Hacout Alexandre <alexandrehacout@orange.fr>
2013-01-23 19:45:36 +0100

    23-01-2013
    19h

--- FuryGame/views/infos/index.php
+++ FuryGame/views/infos/index.php
@@ -8,3 +8,15 @@
 		</div>
 	</div>
 </div>
+<div class="grid_16 background_blanc">
+	<div class="titre">Comment se rendre au Fury-game?</div>
+	<div class="grid_16 principe_tournoi" style=" width:900px; margin-right:20px">
+		<img class="tournoi_soustitre_icone" src="<?php echo BASE_URL;?>/img/icones/1358450134_games.png" title="Guilde contre guilde" alt="Guilde contre guilde" />
+		<div class="tournoi_description">
+			Le système des parties rapides est simple et vous permet d'affronter un ou plusieurs joueurs autour d'un jeu.Le gagnant de la partie remporte alors un nombre de points (allant de 5 à 50 points en fonction des jeux, voir système de points.Après avoir fini, il vous est libre de recommencer une autre partie du même jeu ou d'en choisir un autre. 
+			Le système des parties rapides est simple et vous permet d'affronter un ou plusieurs joueurs autour d'un jeu.Le gagnant de la partie remporte alors un nombre de points (allant de 5 à 50 points en fonction des jeux, voir système de points.Après avoir fini, il vous est libre de recommencer une autre partie du même jeu ou d'en choisir un autre. 
+			</br>
+			<img class="" src="<?php echo BASE_URL;?>/img/plan_geographique.jpg" title="Guilde contre guilde" alt="Guilde contre guilde" />
+		</div>
+	</div>
+</div>
