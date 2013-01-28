3887d672d755d5cb491e688cc4e8de5eb3880c3d
HAcout alexandre <helldjayk@gmail.com>
2013-01-28 16:25:16 +0100

    28-01-2013
    16h30

--- FuryGame/views/fury_games/index.php
+++ FuryGame/views/fury_games/index.php
@@ -1,4 +1,16 @@
 <div class="grid_16 background_blanc">
+	<div class="grid_3 icone_article">
+		<img class="tournoi_soustitre_icone" src="<?php echo BASE_URL;?>/img/petit-logo-fury-game.png" title="Guilde contre guilde" alt="Guilde contre guilde" />
+	</div>
+	<div class="grid_13 principe_tournoi" style=" width:700px; margin-right:20px">
+		<div class="titre_articles">Les principales activitées du Fury-Game</div>
+		<div class="tournoi_description">
+			Le système des parties rapides est simple et vous permet d'affronter un ou plusieurs joueurs autour d'un jeu.Le gagnant de la partie remporte alors un nombre de points (allant de 5 à 50 points en fonction des jeux, voir système de points.Après avoir fini, il vous est libre de recommencer une autre partie du même jeu ou d'en choisir un autre. 
+			Le système des parties rapides est simple et vous permet d'affronter un ou plusieurs joueurs autour d'un jeu.Le gagnant de la partie remporte alors un nombre de points (allant de 5 à 50 points en fonction des jeux, voir système de points.Après avoir fini, il vous est libre de recommencer une autre partie du même jeu ou d'en choisir un autre. 
+		</div>
+	</div>
+</div>
+<div class="grid_16 background_blanc">
 	<div class="titre">Les principales activitées du Fury-Game</div>
 	<div class="grid_16 principe_tournoi" style=" width:900px; margin-right:20px">
 		<img class="tournoi_soustitre_icone" src="<?php echo BASE_URL;?>/img/petit-logo-fury-game.png" title="Le festival fury-game" alt="Le festival fury-game" />
--- FuryGame/views/le_tournois/index.php
+++ FuryGame/views/le_tournois/index.php
@@ -3,7 +3,7 @@
 	<div class="container_16">
 		<div class="grid_8 principe_tournoi" style=" width:430px; margin-right:20px">
 			<div class="tournoi_soustitre">Le principe</div>
-			<img class="tournoi_soustitre_icone" src="<?php echo BASE_URL;?>/img/icones/1358450205_Games2.png" title="Le principe du tournoi" alt="Le principe du tournoi" />
+			<img class="tournoi_soustitre_icone" style="margin-right:20px"src="<?php echo BASE_URL;?>/img/icones/1358450205_Games2.png" title="Le principe du tournoi" alt="Le principe du tournoi" />
 			
 			<div class="tournoi_description">
 				Le système des parties rapides est simple et vous permet d'affronter un ou plusieurs joueurs autour d'un jeu.Le gagnant de la partie remporte alors un nombre de points (allant de 5 à 50 points en fonction des jeux, voir système de points.Après avoir fini, il vous est libre de recommencer une autre partie du même jeu ou d'en choisir un autre. 
@@ -11,7 +11,7 @@
 		</div>
 		<div class="grid_8 principe_tournoi" style=" width:440px; margin-left:0px; margin-right:10px;">
 			<div class="tournoi_soustitre"><span>Pour qui?</span></div>
-			<img class="tournoi_soustitre_icone" src="<?php echo BASE_URL;?>/img/icones/1358450703_Adobe Help Center.png" title="Pour qui?" alt="Pour qui?" />
+			<img class="tournoi_soustitre_icone" style="margin-right:20px" src="<?php echo BASE_URL;?>/img/icones/1358450703_Adobe Help Center.png" title="Pour qui?" alt="Pour qui?" />
 			
 			<div class="tournoi_description">
 				Le système des parties rapides est simple et vous permet d'affronter un ou plusieurs joueurs autour d'un jeu.Le gagnant de la partie remporte alors un nombre de points (allant de 5 à 50 points en fonction des jeux, voir système de points.Après avoir fini, il vous est libre de recommencer une autre partie du même jeu ou d'en choisir un autre. 
@@ -19,7 +19,7 @@
 		</div>
 		<div class="grid_8 principe_tournoi" style=" width:430px; margin-right:20px">
 			<div class="tournoi_soustitre"><span>Comment jouer?</span></div>
-			<img class="tournoi_soustitre_icone" src="<?php echo BASE_URL;?>/img/icones/1358450117_package_games_board.png" title="Comment jouer?" alt="Comment jouer?" />
+			<img class="tournoi_soustitre_icone" style="margin-right:20px" src="<?php echo BASE_URL;?>/img/icones/1358450117_package_games_board.png" title="Comment jouer?" alt="Comment jouer?" />
 			
 			<div class="tournoi_description">
 				Le système des parties rapides est simple et vous permet d'affronter un ou plusieurs joueurs autour d'un jeu.Le gagnant de la partie remporte alors un nombre de points (allant de 5 à 50 points en fonction des jeux, voir système de points.Après avoir fini, il vous est libre de recommencer une autre partie du même jeu ou d'en choisir un autre. 
@@ -27,7 +27,7 @@
 		</div>
 		<div class="grid_8 principe_tournoi" style="  width:440px; margin-left:0px; margin-right:10px;">
 			<div class="tournoi_soustitre"><span>Les jeux</span></div>
-			<img class="tournoi_soustitre_icone" src="<?php echo BASE_URL;?>/img/icones/1358450195_dossier jeux old school.png" title="Les jeux du tournoi" alt="Les jeux du tournoi" />
+			<img class="tournoi_soustitre_icone" style="margin-right:20px" src="<?php echo BASE_URL;?>/img/icones/1358450195_dossier jeux old school.png" title="Les jeux du tournoi" alt="Les jeux du tournoi" />
 			
 			<div class="tournoi_description">
 				Le système des parties rapides est simple et vous permet d'affronter un ou plusieurs joueurs autour d'un jeu.Le gagnant de la partie remporte alors un nombre de points (allant de 5 à 50 points en fonction des jeux, voir système de points.Après avoir fini, <a href="<?php echo BASE_URL;?>/le_tournois/liste_des_jeux"  title="Liste des jeux TFG" alt="liste des jeux TFG">Voir la liste des jeux du tournois F-G.</a>
@@ -52,7 +52,7 @@
 	<div class="container_16">
 		<div class="grid_8 principe_tournoi" style=" width:430px; margin-right:20px">
 			<div class="tournoi_soustitre">Le principe</div>
-			<img class="tournoi_soustitre_icone" src="<?php echo BASE_URL;?>/img/icones/1358450205_Games2.png" title="Le principe du tournoi" alt="Le principe du tournoi" />
+			<img class="tournoi_soustitre_icone" style="margin-right:20px"src="<?php echo BASE_URL;?>/img/icones/1358450205_Games2.png" title="Le principe du tournoi" alt="Le principe du tournoi" />
 			
 			<div class="tournoi_description">
 				Le système des parties rapides est simple et vous permet d'affronter un ou plusieurs joueurs autour d'un jeu.Le gagnant de la partie remporte alors un nombre de points (allant de 5 à 50 points en fonction des jeux, voir système de points.Après avoir fini, il vous est libre de recommencer une autre partie du même jeu ou d'en choisir un autre. 
@@ -60,7 +60,7 @@
 		</div>
 		<div class="grid_8 principe_tournoi" style=" width:440px; margin-left:0px; margin-right:10px;">
 			<div class="tournoi_soustitre"><span>Pour qui?</span></div>
-			<img class="tournoi_soustitre_icone" src="<?php echo BASE_URL;?>/img/icones/1358450703_Adobe Help Center.png" title="Pour qui?" alt="Pour qui?" />
+			<img class="tournoi_soustitre_icone" style="margin-right:20px" src="<?php echo BASE_URL;?>/img/icones/1358450703_Adobe Help Center.png" title="Pour qui?" alt="Pour qui?" />
 			
 			<div class="tournoi_description">
 				Le système des parties rapides est simple et vous permet d'affronter un ou plusieurs joueurs autour d'un jeu.Le gagnant de la partie remporte alors un nombre de points (allant de 5 à 50 points en fonction des jeux, voir système de points.Après avoir fini, il vous est libre de recommencer une autre partie du même jeu ou d'en choisir un autre. 
@@ -68,7 +68,7 @@
 		</div>
 		<div class="grid_8 principe_tournoi" style=" width:430px; margin-right:20px">
 			<div class="tournoi_soustitre"><span>Comment jouer?</span></div>
-			<img class="tournoi_soustitre_icone" src="<?php echo BASE_URL;?>/img/icones/1358450117_package_games_board.png" title="Comment jouer?" alt="Comment jouer?" />
+			<img class="tournoi_soustitre_icone" style="margin-right:20px" src="<?php echo BASE_URL;?>/img/icones/1358450117_package_games_board.png" title="Comment jouer?" alt="Comment jouer?" />
 			
 			<div class="tournoi_description">
 				Le système des parties rapides est simple et vous permet d'affronter un ou plusieurs joueurs autour d'un jeu.Le gagnant de la partie remporte alors un nombre de points (allant de 5 à 50 points en fonction des jeux, voir système de points.Après avoir fini, il vous est libre de recommencer une autre partie du même jeu ou d'en choisir un autre. 
@@ -76,10 +76,10 @@
 		</div>
 		<div class="grid_8 principe_tournoi" style="  width:440px; margin-left:0px; margin-right:10px;">
 			<div class="tournoi_soustitre"><span>Les jeux</span></div>
-			<img class="tournoi_soustitre_icone" src="<?php echo BASE_URL;?>/img/icones/1358450195_dossier jeux old school.png" title="Les jeux du tournoi" alt="Les jeux du tournoi" />
+			<img class="tournoi_soustitre_icone" style="margin-right:20px" src="<?php echo BASE_URL;?>/img/icones/1358450195_dossier jeux old school.png" title="Les jeux du tournoi" alt="Les jeux du tournoi" />
 			
 			<div class="tournoi_description">
-				Le système des parties rapides est simple et vous permet d'affronter un ou plusieurs joueurs autour d'un jeu.Le gagnant de la partie remporte alors un nombre de points (allant de 5 à 50 points en fonction des jeux, voir système de points.Après avoir fini, il vous est libre de recommencer une autre partie du même jeu ou d'en choisir un autre. 
+				Le système des parties rapides est simple et vous permet d'affronter un ou plusieurs joueurs autour d'un jeu.Le gagnant de la partie remporte alors un nombre de points (allant de 5 à 50 points en fonction des jeux, voir système de points.Après avoir fini, <a href="<?php echo BASE_URL;?>/le_tournois/liste_des_jeux"  title="Liste des jeux TFG" alt="liste des jeux TFG">Voir la liste des jeux du tournois F-G.</a>
 			</div>
 		</div>
 	</div>
